4ab432d12c90041bbc54e774e2087b92a7fe54b3
Shelepov Anton <antonsh1@mail.ru>
2023-03-05 20:54:55 +0500

    Удален мусор, добавлены комментарии, финальный вариант

--- src/main/java/ru/smartjava/Main.java
+++ src/main/java/ru/smartjava/Main.java
@@ -1,8 +1,9 @@
 package ru.smartjava;
 
-import ru.smartjava.classes.ReadFromSocket;
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Objects;
@@ -10,29 +11,29 @@ import java.util.Objects;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Сервер запущен");
-        String serverName = "netology.homework";
-        ReadFromSocket readFromSocket = new ReadFromSocket();
-        int port = 8090;
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
 
+        String serverName = "netology.homework";
+        int port = 8090;
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
             while (true) {
                 try (Socket clientSocket = serverSocket.accept();
                      BufferedReader in = new BufferedReader(new InputStreamReader((clientSocket.getInputStream())));
-                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true);
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true)
                 ) {
+
                     System.out.printf("Новое соединение установлено порт %d%n", clientSocket.getPort());
+                    //Уведомляем куда подключился клиент
                     out.printf("Вы подключились к серверу %s%n", serverName);
-                    System.out.println("Вопрос 1");
-                    out.println("Вопрос: Write your name?");
+                    //Приставка "Вопрос:" в тексте сообщения говорит удаленной стороне, что требуется ответ
+                    out.println("Вопрос:Write your name");
                     String name = in.readLine();
-                    System.out.println("Name = " + name);
-                    System.out.println("Сообщаем порт");
                     out.println("---------------------------------------");
                     out.printf("Привет %s, ваш порт %d%n", name, clientSocket.getPort());
                     out.println("---------------------------------------");
+                    //Добиваемся от клиента четкого ответа yes/no
                     while(true) {
-                        System.out.println("Вопрос 2");
-                        out.println("Вопрос: Are you child? (yes/no)");
+                        out.println("Вопрос:Are you child? (yes/no)");
                         String resp = in.readLine();
                         System.out.println(resp);
                         if(Objects.equals(resp, "yes")) {
--- src/main/java/ru/smartjava/client/client.java
+++ src/main/java/ru/smartjava/client/client.java
@@ -1,7 +1,5 @@
 package ru.smartjava.client;
 
-import ru.smartjava.classes.ReadFromSocket;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,15 +7,15 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.Arrays;
 
 public class client {
     public static void main(String[] args) {
         String host = "netology.homework";
-        ReadFromSocket readFromSocket = new ReadFromSocket();
-
+        String hostIp;
+        //Получаем ip хоста
         try {
             InetAddress inetAddress = InetAddress.getByName(host);
+            hostIp = inetAddress.getHostAddress();
             System.out.println();
             System.out.println(host + ", ip address: " + inetAddress.getHostAddress());
             System.out.println();
@@ -26,18 +24,20 @@ public class client {
         }
 
         int port = 8090;
-        try(Socket clientSocket = new Socket(host,port);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true);
-            BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
-        )
-        {
-            while(true) {
+        try (Socket clientSocket = new Socket(hostIp, port);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader console = new BufferedReader(new InputStreamReader(System.in))
+        ) {
+            //бесконечный цикл чтения с удаленной стороны
+            while (true) {
                 String resp = in.readLine();
-                if(resp == null) {
+                //Выходим если null
+                if (resp == null) {
                     return;
                 }
-                if(resp.contains("Вопрос:")){
+                //Если присутствует вопрос, запрашиваем у пользователя ввод строки
+                if (resp.contains("Вопрос:")) {
                     System.out.println(resp.split(":")[1]);
                     System.out.print("> ");
                     String inputString = console.readLine();
@@ -45,10 +45,7 @@ public class client {
                 } else {
                     System.out.println(resp);
                 }
-
-
             }
-
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
